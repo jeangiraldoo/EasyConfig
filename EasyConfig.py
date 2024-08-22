974c918f2ed4c1bf62168fc967f822066a81f8b1
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2024-08-21 19:51:05 -0500

    Create the core of the application. Add the basic "system" command that shows system-specific information

---
+++ EasyConfig.py
@@ -0,0 +1,22 @@
+import platform 
+import os 
+from argparse import ArgumentParser
+
+def showSystemInfo():
+    operatingSystem = platform.system()
+    user = os.getlogin()
+    print(f"Operating system: {operatingSystem}")
+    print(f"User: {user}")
+
+parser = ArgumentParser(prog = "EasyConfig",description = "Automates the process of managing and installing configuration files")
+
+subparsers = parser.add_subparsers(dest = "command")
+subparser_system = subparsers.add_parser("system", help = "Returns system-specific information")
+subparser_system.set_defaults(func = showSystemInfo)
+
+args = parser.parse_args() 
+
+if hasattr(args, "func"):
+    args.func()
+
+
