26c553ea6d34a9ca3b5b5dd51396eb1c94387695
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2024-08-22 22:28:52 -0500

    Add the create command with a -p flag. This flag should allow the user to specify a software name and a path for the configuration file for future use.

--- EasyConfig.py
+++ EasyConfig.py
@@ -9,11 +9,28 @@ def showSystemInfo(args):
         print(f"Operating system: {operatingSystem}")
         print(f"User: {user}")
 
-parser = ArgumentParser(prog = "EasyConfig",description = "Automates the process of managing and installing configuration files")
+def create(args, parser):
+    if(args.software_name == "false"):
+        parser.error("The name of the software was not provided")
+    elif(args.p and args.path_name == "false"):
+        errorMessage = "The path was not provided"
+        parser.error(errorMessage)
+    else:
+        paths[args.software_name] = args.path_name
+
+paths = {}
+
+parser = ArgumentParser(prog = "EasyConfig", description = "Automates the process of managing and installing configuration files")
 
 subparsers = parser.add_subparsers(dest = "command")
 subparser_system = subparsers.add_parser("system", help = "Returns system-specific information")
 subparser_system.set_defaults(func = showSystemInfo)
+
+subparser_create = subparsers.add_parser("create", help = "Creates a configuration file or adds a path with the -p flag for future use")
+subparser_create.add_argument("-p", action = "store_true", default = False)
+subparser_create.add_argument("software_name", nargs = "?", default = "false")
+subparser_create.add_argument("path_name", nargs = "?", default = "false")
+subparser_create.set_defaults(func = lambda args: create(args, subparser_create))
 
 args = parser.parse_args() 
 
