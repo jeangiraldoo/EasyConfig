3fc4188c37703985ff8bcbbf52345c9462b36c5a
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2024-08-23 20:26:03 -0500

    Move the commands from the EasyConfig file to their own package ("commands") in order to make the code more readable and scalable

--- EasyConfig.py
+++ EasyConfig.py
@@ -1,6 +1,8 @@
 import platform 
 import os 
 from argparse import ArgumentParser
+from commands import commands
+
 
 def createECFile():
     if(not(os.path.exists("config.ec"))):
@@ -14,61 +16,7 @@ def showSystemInfo(args):
         print(f"Operating system: {operatingSystem}")
         print(f"User: {user}")
 
-def checkRecord(name, path):
-    config = open("config.ec", "r")
-    configContent = config.read()
-    char_num = len(configContent)
-    configContent = configContent[1:(char_num - 1)]
-    
-    stuff = ""
-    for i in configContent:
-        if(not(i == ":" or i == ",")):
-            stuff += i
-        else:
-            if(stuff == name or stuff == path):
-                return True
-            else:
-                stuff = ""
-    return False
 
-def add(args, parser):
-    if(args.p and args.software_name == "false"):
-        parser.error("The name of the software was not provided")
-    elif(args.p and args.path_name == "false"):
-        errorMessage = "The path was not provided"
-        parser.error(errorMessage)
-    elif(args.p and args.path_name != "false"):
-        config = open("config.ec", "r")
-        configContent = config.read()
-        char_num = len(configContent)
-        configContent = configContent[1:(char_num - 1)]
-        entryExists = checkRecord(args.software_name, args.path_name)
-
-        if(entryExists):
-            parser.error("The name or path has been added in the past")
-        else:
-            configContent = f"{args.software_name}:{args.path_name},{configContent}"
-            config = open("config.ec","w")
-            config.write(f"[{configContent}]")
-            print(f"{args.software_name}:{args.path_name} added")
-
-def list_(args):
-    if(args.command == "list" and args.p):
-
-        print("List of added paths:")
-        config = open("config.ec", "r")
-        configContent = config.read()
-        content_len = len(configContent)
-        configContent = configContent[1:(content_len - 1)]
-    
-        string_to_print = ""
-
-        for i in configContent:
-            if(not(i == ",")):
-                string_to_print += i 
-            elif(i == ","):
-                print(f"{string_to_print}\n")
-            
 createECFile()
 
 parser = ArgumentParser(prog = "EasyConfig", description = "Automates the process of managing and installing configuration files")
@@ -81,11 +29,11 @@ subparser_add = subparsers.add_parser("add", help = "Adds a path with the -p fla
 subparser_add.add_argument("-p", action = "store_true", default = False)
 subparser_add.add_argument("software_name", nargs = "?", default = "false")
 subparser_add.add_argument("path_name", nargs = "?", default = "false")
-subparser_add.set_defaults(func = lambda args: add(args, subparser_add))
+subparser_add.set_defaults(func = lambda args: commands.add(args, subparser_add))
 
 subparser_list = subparsers.add_parser("list", help = "Returns the configuration file paths stored")
 subparser_list.add_argument("-p", action = "store_true", default = False)
-subparser_list.set_defaults(func = list_)
+subparser_list.set_defaults(func = commands.list_)
 
 args = parser.parse_args() 
 
---
+++ commands/commands.py
@@ -0,0 +1,55 @@
+def list_(args):
+    if(args.command == "list" and args.p):
+
+        print("List of added paths:")
+        config = open("config.ec", "r")
+        configContent = config.read()
+        content_len = len(configContent)
+        configContent = configContent[1:(content_len - 1)]
+    
+        string_to_print = ""
+
+        for i in configContent:
+            if(not(i == ",")):
+                string_to_print += i 
+            elif(i == ","):
+                print(f"{string_to_print}\n")
+
+def add(args, parser):
+    if(args.p and args.software_name == "false"):
+        parser.error("The name of the software was not provided")
+    elif(args.p and args.path_name == "false"):
+        errorMessage = "The path was not provided"
+        parser.error(errorMessage)
+    elif(args.p and args.path_name != "false"):
+        config = open("config.ec", "r")
+        configContent = config.read()
+        char_num = len(configContent)
+        configContent = configContent[1:(char_num - 1)]
+        entryExists = checkRecord(args.software_name, args.path_name)
+
+        if(entryExists):
+            parser.error("The name or path has been added in the past")
+        else:
+            configContent = f"{args.software_name}:{args.path_name},{configContent}"
+            config = open("config.ec","w")
+            config.write(f"[{configContent}]")
+            print(f"{args.software_name}:{args.path_name} added")
+
+def checkRecord(name, path):
+    config = open("config.ec", "r")
+    configContent = config.read()
+    char_num = len(configContent)
+    configContent = configContent[1:(char_num - 1)]
+    
+    stuff = ""
+    for i in configContent:
+        if(not(i == ":" or i == ",")):
+            stuff += i
+        else:
+            if(stuff == name or stuff == path):
+                return True
+            else:
+                stuff = ""
+    return False
+
