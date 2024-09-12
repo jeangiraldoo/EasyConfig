6e9b7a135ff5bcadc1ebd2bb0a2f912a266ddd1f
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2024-09-11 21:06:29 -0500

    Add parameterized unit tests for verify_path_exists() and get_pairs() functions

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -1,2 +1,21 @@
 import pytest
-from config import manager
+from config import config_manager
+from app_data import easyConfig
+
+@pytest.mark.parametrize("path, expected", [
+    ("Users/Desktop", False), 
+    ("C:/Users", True), 
+    ("ksfakfd", False),
+    ("D:", True)
+])
+def test_verify_path_exists(path, expected):
+    assert config_manager.verify_path_exists(path) == expected
+
+@pytest.mark.parametrize("value, expected", [
+    ("minecraft->C:/Users/{easyConfig.user}/Desktop", ["minecraft", "C:/Users/{easyConfig.user}/Desktop"]),
+    (" my_app ->  C:/Users/{easyConfig.user}/Documents", ["my_app", "C:/Users/{easyConfig.user}/Documents"]),
+    ("single_string", ["single_string"]),
+    ("   single_string_space  ", ["single_string_space"])
+])
+def test_get_pairs(value, expected):
+    assert config_manager.get_pairs(value) == expected
